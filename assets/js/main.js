84b5f3cc3dee59bdcf72b9fbbe7ef8735b15cf3c
Pulkit Singh <singh.pulkit.0076@gmail.com>
2023-03-25 15:10:02 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -291,3 +291,18 @@
   new PureCounter();
 
 })()
+
+var acc = document.getElementsByClassName("accordionfaqs");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
